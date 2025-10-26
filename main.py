19599ac4c2f42097b56e069fd64668758fe751cc
afzp3 <afzp3@umsystem.edu>
2025-10-26 14:13:53 -0500

    fixed indexing and added slicing

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-string_var = ['H','e','l','l','o','!']
+# list
+string_var = ['H','e','l','l','o']
+
+# cascading Hello
 for i in range(len(string_var)):
-    for k in range(i):
+    for k in range(i+1): # i starts at 0
         print(string_var[k], end="")
     print()
+
+# cascading again but with slicing
+# cooler because no nested for-loop
+cooler_string = "World!"
+for i in range(len(cooler_string)):
+    print(cooler_string[:i+1]) # exclusive end index
